cd4888cd5fda0fceafe57a8c7ff0ac9bfbd56f42
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-03-30 12:12:22 +0200

    clearing /docs

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -11,16 +11,16 @@ git pull elewa master
 # build new gitbook
 gitbook build
 # f to overwirte last publication
-cp -rf ../_book/. ../
+cp -rf ./_book/. ../
 # delete the /_book foler
-# rm -rf _book
+rm -rf _book
 # return to parent directory
 # cd ..
 # commit recent build
 git add .
 git commit -m 'publishing'
 # publish changes
-# git push elewa master
+git push elewa master
 
 # to execute this file run this command in terminal:
 # chmod a+x ./publish.sh
