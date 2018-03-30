b49b410f8e540aa0f2e34a402170f3a56f3c20d3
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-03-30 12:09:42 +0200

    clearing /docs

--- src/scripts/publish.sh
+++ src/scripts/publish.sh
@@ -11,7 +11,7 @@ git pull elewa master
 # build new gitbook
 gitbook build
 # f to overwirte last publication
-cp -rf ../_book/. ../
+cp -rf ./_book/. ../
 # delete the /_book foler
 # rm -rf _book
 # return to parent directory
